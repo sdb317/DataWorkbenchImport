d34973a4680f9a559d354c2783a5a68945d7a1d8
sbell <simon.bell@epfl.ch>
2018-05-01 11:15:18 +0200

    Upload template

---
+++ UploadAllTemplate.bat
@@ -0,0 +1,9 @@
+::SpecimenGroup
+call Upload.bat "<session folder>\DataWorkbench\<Specimen file>" SpecimenGroup Rodent 14
+
+::Activity
+call Upload.bat "<session folder>\DataWorkbench\<Activity file>" Activity null 14
+
+::Dataset
+call Upload.bat "<session folder>\DataWorkbench\<Dataset file>" Dataset null 14
+
